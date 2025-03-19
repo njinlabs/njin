bbfcbe6aadc524952633f92f20b9562a03ef2dd6
Akbar Aditama <akbaraditamasp@gmail.com>
2025-03-20 00:43:06 +0700

    feat: add delete & update user

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -1,15 +1,43 @@
 import { Hono } from "hono";
-import validator from "../middlewares/validator";
-import { createUserValidation } from "../validations/user";
 import User from "../database/entities/user";
+import validator from "../middlewares/validator";
 import { successResponse } from "../utils/response";
 import {
   metaDataValidation,
   uuidParamValidation,
 } from "../validations/general";
-import z from "zod";
+import {
+  createUserValidation,
+  updateUserValidation,
+} from "../validations/user";
 
 const user = new Hono();
+
+user.delete("/:id", validator("param", uuidParamValidation), async (c) => {
+  const user = await User.findOneByOrFail({ id: c.req.param("id") });
+  await user.remove();
+
+  return c.json(successResponse("User deleted", user.serialize()));
+});
+
+user.put(
+  "/:id",
+  validator("param", uuidParamValidation),
+  validator("json", updateUserValidation),
+  async (c) => {
+    const user = await User.findOneByOrFail({ id: c.req.param("id") });
+
+    const { fullname, password, email } = await c.req.valid("json");
+
+    user.fullname = fullname;
+    user.plainPassword = password;
+    if (user.email.toLowerCase() !== email.toLowerCase()) user.email = email;
+
+    await user.save();
+
+    return c.json(successResponse("User updated", user.serialize()));
+  }
+);
 
 user.get("/:id", validator("param", uuidParamValidation), async (c) => {
   const user = await User.findOneByOrFail({ id: c.req.param("id") });
@@ -32,7 +60,7 @@ user.get("/", validator("query", metaDataValidation), async (c) => {
   const { perPage, page } = await c.req.valid("query");
 
   const users = await User.find({
-    take: page,
+    take: perPage,
     skip: (page - 1) * perPage,
   });
   const usersCount = await User.count();
--- src/validations/user.ts
+++ src/validations/user.ts
@@ -1,5 +1,10 @@
-import { password } from "bun";
 import z from "zod";
+
+export const updateUserValidation = z.object({
+  fullname: z.string().nonempty(),
+  email: z.string().email().nonempty(),
+  password: z.coerce.string(),
+});
 
 export const createUserValidation = z.object({
   fullname: z.string().nonempty(),
